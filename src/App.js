adb0efae1713536ebe13b8f333cf035049e2e22e
Adiination <Aditya.sasaram11@gmail.com>
2023-04-20 13:02:12 +0530

    installed required dependancies and creating folder structures

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
+import Dp from './components/pictures/Dp.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <img src = {Dp} alt = "My picture" id = "pic"></img>
+
       
     </div>
   );
