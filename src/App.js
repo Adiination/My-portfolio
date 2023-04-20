cc028831012614467b378f0988ead728cedfe7e2
Adiination <Aditya.sasaram11@gmail.com>
2023-04-20 13:51:02 +0530

    Routing pages and creating files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import Dp from './components/pictures/Dp.jpg';
 import './App.css';
+import About from "./Routes/About";
+import Contact from "./Routes/Contact";
+import Home from "./Routes/Home";
+import Project from "./Routes/Project";
+import { Routes,Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <img src = {Dp} alt = "My picture" id = "pic"></img>
+    <>
+    <Routes>
+      <Route path='/' element = {<Home/>}/>
+      <Route path='/Contact' element = {<Contact/>}/>
+      <Route path='/Project' element = {<Project/>}/>
+      <Route path='/About' element = {<About/>}/>
 
-      
-    </div>
+
+
+
+    </Routes>
+    
+    
+    
+    </>
   );
 }
 
